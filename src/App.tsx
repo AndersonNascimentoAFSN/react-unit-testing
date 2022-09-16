61512e6f06b71f888a9bf508ec1655881c273dda
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-09-15 22:52:55 -0300

    create environment tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 export function App() {
+  const [people, setPeople] = useState("");
+  const [peoples, setPeoples] = useState<string[]>([]);
+
+  function handleAddPeoples() {
+    if (!people) return;
+    setPeoples((peoples) => [...peoples, people]);
+    setPeople("");
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Family</h1>
+      <input
+        type="text"
+        onChange={(event) => setPeople(event.target.value)}
+        value={people}
+      />
+      <ul>
+        {peoples.map((people, index) => (
+          <li key={index}>{people}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddPeoples}>Add People</button>
     </div>
   );
 }
